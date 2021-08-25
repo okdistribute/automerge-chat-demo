5a7e1a497277da4804f179d43788f771e599c722
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-08-25 12:22:03 -0700

    standard style

--- bin/server.js
+++ bin/server.js
@@ -1,81 +1,81 @@
-var WebSocketServer = require('websocket').server
-var http = require('http');
-var crypto = require('crypto');
+const WebSocketServer = require('websocket').server
+const http = require('http')
+const crypto = require('crypto')
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
+const server = http.createServer(function (request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url)
+  response.writeHead(404)
+  response.end()
+})
 
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+server.listen(8080, function () {
+  console.log((new Date()) + ' Server is listening on port 8080')
+})
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+let wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false
+})
 
-function originIsAllowed(origin) {
-	// put logic here to detect whether the specified origin is allowed.
-	return true;
- }
+function originIsAllowed (origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true
+}
 
 class Peer {
-	constructor (connection) {
-		this.id = crypto.randomBytes(16).toString('hex');
-		this.connection = connection;
-		this.stale = false;
-	}
+  constructor (connection) {
+    this.id = crypto.randomBytes(16).toString('hex')
+    this.connection = connection
+    this.stale = false
+  }
 }
 
 const pipeOneWay = (A, B) => {
-	let onmessage = data => {
-		B.sendBytes(data.binaryData);
-	}
-	const cleanup = () => {
-		A.close()
-		B.close()
-		A.removeListener('message', onmessage)
-		A.removeListener('error', cleanup)
-		A.removeListener('close', cleanup)
-	}
-	A.on('message', onmessage)
-	A.on('error', cleanup)
-	A.on('close', cleanup)
+  const onmessage = data => {
+    B.sendBytes(data.binaryData)
+  }
+  const cleanup = () => {
+    A.close()
+    B.close()
+    A.removeListener('message', onmessage)
+    A.removeListener('error', cleanup)
+    A.removeListener('close', cleanup)
+  }
+  A.on('message', onmessage)
+  A.on('error', cleanup)
+  A.on('close', cleanup)
 }
 
-const documents = new Map() 
+const documents = new Map()
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+wsServer.on('request', function (request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject()
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.')
+    return
+  }
+  const connection = request.accept('echo-protocol', request.origin)
+  const peers = documents.get(request.resource) || []
+  console.log('get peers', peers.length)
+  const me = new Peer(connection)
+  peers.forEach(p => {
+    if (!p.stale) {
+      pipeOneWay(p.connection, me.connection)
+      pipeOneWay(me.connection, p.connection)
     }
-	connection = request.accept('echo-protocol', request.origin);
-	let peers = documents.get(request.resource) || []
-	console.log('get peers', peers.length)
-	let me = new Peer(connection)
-	peers.forEach(p => {
-		if (!p.stale) {
-			pipeOneWay(p.connection, me.connection)
-			pipeOneWay(me.connection, p.connection)
-		}
-	})
-	peers.push(me)
-    connection.on('close', function(reasonCode, description) {
-		me.stale = true
-		documents.set(request.resource, peers.filter(p => !p.stale))
-		console.log(documents.get(request.resource).length)
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-	documents.set(request.resource, peers)
-});
+  })
+  peers.push(me)
+  connection.on('close', function (reasonCode, description) {
+    me.stale = true
+    documents.set(request.resource, peers.filter(p => !p.stale))
+    console.log(documents.get(request.resource).length)
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.')
+  })
+  documents.set(request.resource, peers)
+})
